fd73c5dc4e169688f06f764988978b2afb48a3f5
bciachurski <b.ciachurski@gmail.com>
2019-06-03 20:25:35 +0200

    Pierwsza wizualizacja panelu po zalogowaniu

--- resources/views/panel.blade.php
+++ resources/views/panel.blade.php
@@ -1,4 +1,18 @@
 @extends('master')
 @section('content')
 
+<div class="container box-bialy">
+	<div class="row">
+		<div class="col-md-4">
+			<h2 class="h2-panel">Opcje</h2>
+		</div>
+		<div class="col-md-8">
+			<div class="block-right">
+				<p class="opis-panel">Logując się do naszego panelu jesteś w stanie zamówić właśny bilet online do zoo</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ultricies dui non nunc iaculis, at porta ex imperdiet. Sed venenatis ipsum in ullamcorper vehicula. Suspendisse sit amet orci leo. Phasellus id sapien nisl. Nam sagittis tincidunt quam, aliquet rutrum augue mollis eu. Fusce quis lorem porttitor, tincidunt leo nec, pulvinar dolor. Morbi ac metus magna. Duis vulputate nisl dignissim diam egestas tincidunt. Curabitur ut magna diam. Nam mattis leo tristique fringilla aliquet. Curabitur tempor, augue at mollis accumsan, purus diam vulputate augue, vitae malesuada magna mi eu nulla.</p>
+			</div>
+		</div>
+	</div>
+</div>
+
 @endsection
